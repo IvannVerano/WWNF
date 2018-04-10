51332d3d27c4a1685313354a2eb39bf45197806e
antoniorv6 <antoniorv6@gmail.com>
2018-04-10 13:47:10 +0200

    soundmanager incluido en la alfa

--- AssetManager.hpp
+++ AssetManager.hpp
@@ -2,23 +2,31 @@
 
 #include <map>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 namespace Zenon {
 
-    class AssetManager
-	{
-	public:
-		AssetManager() {}
-		~AssetManager() {}
+    class AssetManager {
+    public:
 
-		void LoadTexture(std::string name, std::string fileName);
-		sf::Texture &GetTexture(std::string name);
+        AssetManager() {
+        }
 
-		void LoadFont(std::string name, std::string fileName);
-		sf::Font &GetFont(std::string name);
+        ~AssetManager() {
+        }
 
-	private:
-		std::map<std::string, sf::Texture> _textures;
-		std::map<std::string, sf::Font> _fonts;
-	};
+        void LoadTexture(std::string name, std::string fileName);
+        sf::Texture &GetTexture(std::string name);
+
+        void LoadFont(std::string name, std::string fileName);
+        sf::Font &GetFont(std::string name);
+
+        void LoadBuffer(std::string name, std::string fileName);
+        sf::SoundBuffer &GetBuffer(std::string name);
+
+    private:
+        std::map<std::string, sf::Texture> _textures;
+        std::map<std::string, sf::Font> _fonts;
+        std::map<std::string, sf::SoundBuffer> m_buffers;
+    };
 }
